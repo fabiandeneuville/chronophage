6fae8b9a33b0cd5a9e435f5e3a33c9888acdc837
Deneuville Fabian <deneuville.fabian@live.fr>
2022-06-18 12:50:17 +0200

    Adding day

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const secondsHand = document.querySelector('.secondsHand');
 const minutesHand = document.querySelector('.minutesHand');
-const hoursHand = document.querySelector('.hoursHand')
+const hoursHand = document.querySelector('.hoursHand');
+const dayScreen = document.querySelector('.dayScreen');
+const dayName = document.querySelector('.dayName');
 
 function setDate(){
     const now = new Date();
@@ -12,11 +14,25 @@ function setDate(){
     const minutesDegrees = ((minutes / 60) * 360);
     const hoursDegrees = ((hours / 12) * 360);
 
-    secondsHand.style.transform = `translateX(-50%) rotate(${secondsDegrees}deg)`
-    minutesHand.style.transform = `translateX(-50%) rotate(${minutesDegrees}deg)`
-    hoursHand.style.transform = `translateX(-50%) rotate(${hoursDegrees}deg)`
+    secondsHand.style.transform = `translateX(-50%) rotate(${secondsDegrees}deg)`;
+    minutesHand.style.transform = `translateX(-50%) rotate(${minutesDegrees}deg)`;
+    hoursHand.style.transform = `translateX(-50%) rotate(${hoursDegrees}deg)`;
+
+    const dayStrOptions = { weekday: 'long'}
+    const dayStr = now.toLocaleDateString('fr-FR', dayStrOptions);
+    dayName.textContent = dayStr;
+
+    const dayNumOptions = { day: 'numeric'}
+    const dayNum = now.toLocaleDateString('fr-FR', dayNumOptions);
+    dayScreen.textContent = dayNum
 }
 
 setInterval(setDate, 1000);
 
 setDate();
+
+const now = new Date();
+const options = { weekday: 'short', month: 'short', day: 'numeric'}
+
+const date = new Date();
+
