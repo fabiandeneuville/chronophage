4b463e87e343b6f10d962505046ff197d2deae52
Deneuville Fabian <deneuville.fabian@live.fr>
2022-06-19 16:46:59 +0200

    Improving hours display

--- app.js
+++ app.js
@@ -11,8 +11,8 @@ function setDate(){
     const hours = now.getHours();
 
     const secondsDegrees = ((seconds / 60) * 360);
-    const minutesDegrees = ((minutes / 60) * 360);
-    const hoursDegrees = ((hours / 12) * 360);
+    const minutesDegrees = ((minutes / 60) * 360) + ((seconds / 60) * 6);
+    const hoursDegrees = ((hours / 12) * 360) + ((minutes / 60) * 30);
 
     secondsHand.style.transform = `translateX(-50%) rotate(${secondsDegrees}deg)`;
     minutesHand.style.transform = `translateX(-50%) rotate(${minutesDegrees}deg)`;
